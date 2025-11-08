87e588dce9a753db5295203ec8141417267d6536
Lakshith <lakshith.achuta@gmail.com>
2025-11-08 16:35:06 +0530

    Restructed the entire backend along with schema

---
+++ backend/company/services.js
@@ -0,0 +1,20 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const createCompanyifnotExists = async (companyData) => {
+  const { email, companyName } = companyData;
+  const existingCompany = await prisma.companies.findFirst({
+    where: { OR: [{ email }, { companyName }] }
+  });
+
+  if (existingCompany) {
+    throw new Error('Company with this email or company name already exists');
+  }
+
+  const newCompany = await prisma.companies.create({
+    data: companyData
+  });
+
+  return newCompany;
+}
+module.exports = { createCompanyifnotExists };
